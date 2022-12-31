68dab0ff886e2825f419ca712685ff29787d87e9
Kristin Brewer <kr527106@gmail.com>
2022-12-31 14:28:41 -0500

    modified questions, added more detailed license info

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# README GENERATOR
+# README
 
-[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 ## Description
-generates read mes
+generates readmes
 
 ## Table of Contents
 - [Description](#description)
@@ -15,21 +15,21 @@ generates read mes
 - [Questions](#questions)
 
 ## Installation
-none
+N/a
 
 ## Usage
 none
 
 ## License
-ISC
-This application is covered by the ISC license
+Apache License 2.0 Copyright (c) 2022 kristinbrewer
+Licensed under the Apache License, Version 2.0 (the ”License”); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an ”AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 ## Contributing
-me
-
-## Tests
 none
 
+## Tests
+this
+
 ## Questions
-For more information, my GitHub account is: [github.com/kristinbrewer](https://github.com/kristinbrewer) .
+For more information, my GitHub account is: [kristinbrewer](https://kristinbrewer) .
 Please email me at: kr527106@gmail.com with any additional questions. 
--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-//const questions = [];
-
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
-
-
-///starting new code 
+//packages for application 
 const fs = require('fs');
 const inquirer = require('inquirer');
 
 function createREADME(userInfo) {
+    //userinput vars
     let {
         badge,
         projectTitle,
@@ -27,16 +18,16 @@ function createREADME(userInfo) {
         email,
 
     } = userInfo;
-
+//if statement for license information 
     if (license === "MIT"){
         badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-        licenseInfo = "This application is covered by the MIT license";
+        licenseInfo = "Permission is hearby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED \”AS IS\”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. ";
     } else if (license === "ISC") {
         badge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-        licenseInfo = "This application is covered by the ISC license";
+        licenseInfo = "Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies. THE SOFTWARE IS PROVIDED \”AS IS\” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.";
     } else {
         badge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-        licenseInfo = "This application is covered by the Apache 2.0 license";
+        licenseInfo = "Licensed under the Apache License, Version 2.0 (the \”License\”); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \”AS IS\” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.";
     }
 
     //return template for README generation 
@@ -63,7 +54,7 @@ ${installation}
 ${usage}
 
 ## License
-${license}
+${license} Copyright (c) 2022 ${username}
 ${licenseInfo}
 
 ## Contributing
@@ -73,10 +64,10 @@ ${contributing}
 ${tests}
 
 ## Questions
-For more information, my GitHub account is: [${username}](https://${username}) .
+For more information, my GitHub account is: [${username}](https://github.com/${username}) .
 Please email me at: ${email} with any additional questions. `;
 }
-
+//questions in prompt 
 function askQuestions () {
     return inquirer.prompt( [
         {
@@ -96,7 +87,7 @@ function askQuestions () {
         },
         {
             name: 'usage',
-            message: 'What is the usage?',
+            message: 'How do you use your application?',
             type: 'input',
         },
         {
@@ -107,7 +98,7 @@ function askQuestions () {
         },
         {
             name: 'contributing',
-            message: 'Who is contributing?',
+            message: 'Who contributed to this project?',
             type: 'input',
         },
         {
@@ -117,7 +108,7 @@ function askQuestions () {
         },
         {
             name: 'username',
-            message: 'Please enter your GitHub account:',
+            message: 'Please enter your GitHub user name:',
             type: 'input',
         },
         {
@@ -137,7 +128,7 @@ function writeFile(md) {
 }
 
 
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     askQuestions()
         .then(createREADME)
